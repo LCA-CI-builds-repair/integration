f698d81f18637a631a80013d26cd5ab51b6fadb8
ubuntu <ubuntu@ip-10-0-1-43>
2025-01-30 15:06:36 +0000

    waleko__deepseek-r1_df21__id_60

--- tests/repositories/test_get_documentation.py
+++ tests/repositories/test_get_documentation.py
@@ -6,8 +6,6 @@ from custom_components.hacs.repositories.base import HacsRepository
 
 from tests.common import client_session_proxy
 
-
-
 @pytest.mark.parametrize("data,result", [
     ({"installed": True, "installed_version": "1.0.0"}, "Example readme file"),
     ({"installed": False, "last_version": "2.0.0"}, "Example readme file")
